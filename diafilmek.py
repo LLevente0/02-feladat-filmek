38b9d6e2c5f7732d2c25285e17933d22ee64e3d2
Ladányi Levente <ladanyi.levente@diak.szbi-pg.hu>
2025-02-24 10:35:09 +0100

    Update diafilmek.py

--- diafilmek.py
+++ diafilmek.py
@@ -3,7 +3,7 @@ with open("filmek.txt", "r", encoding="utf-8") as forrasfajl:
     for sor in forrasfajl:
         adatok = sor.strip().split(';')
         cim = adatok[0]
-        ev = int(adatok[1])
+        ev = [adatok[1]]
         kockak_szama = int(adatok[2])
         szines = adatok[3]
         filmek.append([cim, ev, kockak_szama, szines])
@@ -11,3 +11,7 @@ with open("filmek.txt", "r", encoding="utf-8") as forrasfajl:
 print(f"{filmek=}")
 
 print(f"\n3.2. feladat: Az állományban {len(filmek)} db diafilm adatai szerepelnek.")
+
+
+ev.sort()
+print(ev[0])
