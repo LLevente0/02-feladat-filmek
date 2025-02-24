aad7cd8f84f38ad1f327da963c87510c02719a20
Ladányi Levente <ladanyi.levente@diak.szbi-pg.hu>
2025-02-24 09:47:50 +0100

    as

---
+++ diafilmek.py
@@ -0,0 +1,9 @@
+filmek = []
+with open("filmek.txt", "r", encoding="utf-8") as forrasfajl:
+    for sor in forrasfajl:
+        adatok = sor.strip().split(',')
+        cim = adatok[0]
+        ev = int(adatok[1])
+        kockak_szama = int(adatok[2])
+        szines = adatok[3]
+        filmek.append([cim, ev, kockak_szama, szines])
